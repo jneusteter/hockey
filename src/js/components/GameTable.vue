d5319795313ab80311793d0ab3e42cd2f93d1f16
John Neusteter <jneusteter@gmail.com>
2018-08-07 21:29:08 -0400

    add game table component

---
+++ src/js/components/GameTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row">
+    <div class="col-lg-8">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+         {{games}}
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      games: []
+    }
+  },
+  methods: {
+    loadGames: function () {
+      axios.get('/api/games')
+      .then((res) => {
+        this.games = this.res
+      })
+    }
+  },
+  created: function(){
+    this.loadGames();
+    // polling api every 30 seconds
+    setInterval(function () {
+      this.loadGames();
+    }.bind(this), 30000);
+  },
+
+}
+</script>
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 
 window.qs = require('qs');
+window.axios = require('axios')
+
+Vue.component('game-table', require('./components/GameTable.vue').default);
 
 const app = new Vue ({
   el: '#app',
